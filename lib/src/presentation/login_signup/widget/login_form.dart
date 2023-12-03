f48736015c7181d6065a8aa6c0c797b4df1a5e6e
thaison <thaison.uet@gmail.com>
2023-12-03 21:01:01 +0700

    Prevented hard back on Login screen

--- lib/src/presentation/login_signup/widget/login_form.dart
+++ lib/src/presentation/login_signup/widget/login_form.dart
@@ -20,101 +20,105 @@ class _LoginFormState extends State<LoginForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          // const Text(
-          //   'Login',
-          //   textAlign: TextAlign.left,
-          //   style: TextStyle(
-          //     fontSize: 20,
-          //     color: colorPrimary,
-          //     fontWeight: FontWeight.bold,
-          //   ),
-          // ),
-          // SizedBox(height: CommonConstants.defaultPadding * 2),
-          TextFormField(
-            keyboardType: TextInputType.emailAddress,
-            textInputAction: TextInputAction.next,
-            cursorColor: colorPrimary,
-            onChanged: (email) {
-              setState(() {
-                this.email = email;
-              });
-            },
-            onSaved: (email) {},
-            decoration: InputDecoration(
-              hintText: "Your email",
-              prefixIcon: Padding(
-                padding: EdgeInsets.all(CommonConstants.defaultPadding),
-                child: const Icon(Icons.person),
-              ),
-            ),
-          ),
-          Padding(
-            padding:
-                EdgeInsets.symmetric(vertical: CommonConstants.defaultPadding),
-            child: TextFormField(
-              textInputAction: TextInputAction.done,
-              obscureText: true,
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Form(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            // const Text(
+            //   'Login',
+            //   textAlign: TextAlign.left,
+            //   style: TextStyle(
+            //     fontSize: 20,
+            //     color: colorPrimary,
+            //     fontWeight: FontWeight.bold,
+            //   ),
+            // ),
+            // SizedBox(height: CommonConstants.defaultPadding * 2),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              textInputAction: TextInputAction.next,
               cursorColor: colorPrimary,
-              onChanged: (password) {
+              onChanged: (email) {
                 setState(() {
-                  this.password = password;
+                  this.email = email;
                 });
               },
+              onSaved: (email) {},
               decoration: InputDecoration(
-                hintText: "Your password",
+                hintText: "Your email",
                 prefixIcon: Padding(
                   padding: EdgeInsets.all(CommonConstants.defaultPadding),
-                  child: const Icon(Icons.lock),
+                  child: const Icon(Icons.person),
                 ),
               ),
             ),
-          ),
-          SizedBox(height: CommonConstants.defaultPadding),
-          SizedBox(
-            width: double.infinity,
-            height: 48,
-            child: ElevatedButton(
-              style: ButtonStyle(
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12.0),
-                          side: const BorderSide(color: Colors.transparent)))),
-              onPressed: isValidForm()
-                  ? () {
-                      AppUtil.instance.isLogin = true;
-                      Navigator.of(context).pushNamedAndRemoveUntil(
-                        home,
-                        (route) => false,
-                      );
-                    }
-                  : null,
-              child: Text(
-                "Login".toUpperCase(),
-                style: const TextStyle(
-                    color: Colors.white, fontWeight: FontWeight.bold),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                  vertical: CommonConstants.defaultPadding),
+              child: TextFormField(
+                textInputAction: TextInputAction.done,
+                obscureText: true,
+                cursorColor: colorPrimary,
+                onChanged: (password) {
+                  setState(() {
+                    this.password = password;
+                  });
+                },
+                decoration: InputDecoration(
+                  hintText: "Your password",
+                  prefixIcon: Padding(
+                    padding: EdgeInsets.all(CommonConstants.defaultPadding),
+                    child: const Icon(Icons.lock),
+                  ),
+                ),
               ),
             ),
-          ),
-          SizedBox(height: CommonConstants.defaultPadding),
-          AlreadyHaveAnAccountCheck(
-            press: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) {
-                    return const SignUpView();
-                  },
+            SizedBox(height: CommonConstants.defaultPadding),
+            SizedBox(
+              width: double.infinity,
+              height: 48,
+              child: ElevatedButton(
+                style: ButtonStyle(
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12.0),
+                            side:
+                                const BorderSide(color: Colors.transparent)))),
+                onPressed: isValidForm()
+                    ? () {
+                        AppUtil.instance.isLogin = true;
+                        Navigator.of(context).pushNamedAndRemoveUntil(
+                          home,
+                          (route) => false,
+                        );
+                      }
+                    : null,
+                child: Text(
+                  "Login".toUpperCase(),
+                  style: const TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
                 ),
-              );
-            },
-          ),
-        ],
+              ),
+            ),
+            SizedBox(height: CommonConstants.defaultPadding),
+            AlreadyHaveAnAccountCheck(
+              press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return const SignUpView();
+                    },
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
